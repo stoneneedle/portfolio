da70e5028ebdcae0b528c60680774962ee12f27e
stoneneedle <stoneneedle@gmail.com>
2022-08-28 18:14:09 -0400

    Installed MUI and added basic typography/tested a component.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 
+// MUI
+import Button from '@mui/material/Button';
+
 function App() {
   return (
     <div className="App">
       <h1>Test</h1>
+
+      <Button variant="contained">Hello World</Button>
+
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
+import Typography from "@mui/material/Typography";
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Typography>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Typography>
 );
 
