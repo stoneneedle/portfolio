b712c7bffed379dce71695d26b1d805c5688181c
stoneneedle <stoneneedle@gmail.com>
2022-08-28 18:03:21 -0400

    Added pages folder, set up intitial routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
 
 function App() {
   return (
     <div className="App">
-      <p>Test</p>
+      <h1>Test</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
