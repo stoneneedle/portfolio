95b321382962be1b0bfa781e4f38e15d73e1f86a
stoneneedle <stoneneedle@gmail.com>
2022-09-01 22:42:10 -0400

    Removed commented code from index and app

--- src/index.jsx
+++ src/index.jsx
@@ -1,40 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import { BrowserRouter } from "react-router-dom";
-// import App from './App';
-// import Typography from "@mui/material/Typography";
-
-
-
-// import { ThemeProvider, createTheme } from '@mui/material/styles';
-// import CssBaseline from '@mui/material/CssBaseline';
-
-// const theme = createTheme({
-//   palette: {
-//     mode: 'dark',
-//     primary: {
-//       main: '#793fb5',
-//     },
-//     secondary: {
-//       main: '#e8e522',
-//     },
-//   },
-// });
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <ThemeProvider theme={theme}>
-//     <CssBaseline />
-//     <Typography>
-//       <BrowserRouter>
-//         <App />
-//       </BrowserRouter>
-//     </Typography>
-//   </ThemeProvider>
-// );
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -48,14 +11,11 @@ import '@fontsource/roboto/700.css';
 // React-Router
 import { BrowserRouter, } from 'react-router-dom';
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-
       <App />
-
     </BrowserRouter>
   </React.StrictMode>
 );
